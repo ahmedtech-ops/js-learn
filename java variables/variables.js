12ddb377f2aff80e9d4788fadd83df068ae61582
ahmedtech-ops <ahmed7338228@gmail.com>
2026-06-18 15:18:12 +0500

    hello

---
+++ java variables/variables.js
@@ -0,0 +1,16 @@
+const accountID= 34444
+let accountemail="ahmed343@gmail.com"
+var accountpassword="23322"
+accountcity="faisalabad"
+let accountstate;
+
+// accountID=2
+
+console.log(accountID);
+
+// prefer not to use var 
+
+accountemail="ahmed444@gmail.com"
+accountpassword="434343"
+accountcit="lahore"
+console.table([accountID,accountemail,accountpassword,accountcit,accountstate])
