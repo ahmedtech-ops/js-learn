168d052705e6e2e84a0046de5d018163fb42792b
ahmedtech-ops <ahmed7338228@gmail.com>
2026-06-18 15:38:37 +0500

    string iteration

--- loops.js/forloop.js
+++ loops.js/forloop.js
@@ -1,4 +1,12 @@
-for (let ali=2 ; ali<20 ; ali+=2 )
+// for (let ali=2 ; ali<20 ; ali+=2 )
+// {
+//     console.log(ali)
+// }
+
+let ahmed = "ahmedraza";
+
+for( let i=0; i<ahmed.length; i++)
 {
-    console.log(ali)
+    console.log(ahmed[i])
 }
+// console.log(ahmed.length)
