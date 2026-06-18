e2b74825a32f0c6de05f434235b481ba64c28006
ahmedtech-ops <ahmed7338228@gmail.com>
2026-06-18 15:49:39 +0500

    while loop

---
+++ loops.js/whileloop.js
@@ -0,0 +1,17 @@
+// let ahmed=0
+// while(
+//     ahmed<=2
+// )
+// {
+//     console.log(ahmed)
+//     ahmed++
+// }
+
+let ahmed="ahm"
+let ali=0
+while(
+ali<=ahmed.length
+)
+{
+    console.log (ahmed[ali++])
+}
