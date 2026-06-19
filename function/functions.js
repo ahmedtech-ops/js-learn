29c12c9d5a031fa3448dfdad44ac744e951d1f0a
ahmedtech-ops <ahmed7338228@gmail.com>
2026-06-19 16:10:52 +0500

    function

---
+++ function/functions.js
@@ -0,0 +1,31 @@
+// function ahmed(AB,ba){
+// console.log(AB,ba)
+// }
+// ahmed(2,3)
+
+// let ahmed=(a,b)=>{
+// console.log(a,b)
+// }
+// ahmed(2,9)
+
+// function para(...abc) { 
+// console.log(abc)
+//  }
+// para(1,2,3,4,5,6,7,8,9)
+// // Rest operator (...)
+
+function iseven(num){
+    if(num%2==0){
+        return true
+    }
+    
+        return false
+    
+}
+if(iseven(3)){
+    console.log("even")
+  
+}
+else{
+    console.log("odd")
+}
---
+++ loops.js/practice.js
@@ -0,0 +1,45 @@
+// let n=5
+// for(
+//     let i=1;
+//     i<=10;
+//     i++
+
+// )
+// {
+//     console.log(`${n} * ${i} = ${n*i}`)
+// }
+
+// let n=413
+// let sum=0
+// while(
+//     n!=0
+// )
+// {
+//     let remainder=n%10
+//     sum+=remainder
+//      n=n/10
+// }
+
+// console.log(sum)
+
+let user=1
+let n=2
+do{
+    if(
+        user<n
+    ){
+        console.log("too low")
+    }
+    else if(
+        user>n
+    ){
+      console.log("too high")
+    }
+
+
+}
+while(
+    n!=user
+)
+console.log("you won")
+
